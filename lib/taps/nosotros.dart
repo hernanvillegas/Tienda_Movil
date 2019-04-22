7c898cc895a82da4c065aa3736db939c8a5b1a43
unknown <you@example.com>
2019-04-22 04:36:56 -0400

    contenodo en mi archivo nocotros

--- lib/taps/nosotros.dart
+++ lib/taps/nosotros.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
-class AudifonosTabs extends StatelessWidget{
+class NosotrosTabs extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     
 
     return Scaffold(
      appBar: new AppBar(
-       title: new Text('Nosotros...'),
+       title: new Text('Sobre Nosotros...'),
+       
      ),
-     body: new Center(
-       child: new Text('Estamos aprendiedo fluter'),
-     ),
+     
+body: new Stack(
+        children: <Widget>[
+          //agregamos una imagen
+          new Container(
+            margin: const EdgeInsets.all(10.0), //margenes por todos lados
+            width: 350.0, //ancho de la img
+            height: 350.0, //largo de la img
+            child: new Text(''),
+            decoration: new BoxDecoration(
+              image: new DecorationImage(image:new AssetImage("assets/images/img.jpg"),
+              fit: BoxFit.cover
+              
+              ),
+            ),
+          ),
+         
+        ],
+      ),
+
     );
   }
 
 }
+
+
