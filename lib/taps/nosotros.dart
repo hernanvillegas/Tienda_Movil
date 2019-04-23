ffa5ed1f52b707041447e17f1a2d6b2c0980443b
unknown <you@example.com>
2019-04-23 03:24:59 -0400

    eliminacion de archivos no utilizados

--- lib/taps/nosotros.dart
+++ lib/taps/nosotros.dart
@@ -16,11 +16,11 @@ body: new Stack(
           //agregamos una imagen
           new Container(
             margin: const EdgeInsets.all(10.0), //margenes por todos lados
-            width: 130.0, //ancho de la img
-            height: 180.0, //largo de la img
-            child: new Text('Hernan Luis Villegas Llanco \nEste proyecto fue realizado para la compra y venta de dispositivos moviles y asi ayudar a varias personas a vender y/o  comprar lo que necesitemos'),
+            width: 400.0, //ancho de la img
+            height: 300.0, //largo de la img
+            child: new Text('Hernan Luis Villegas Llanco \n\n\nEste proyecto fue realizado para la compra y venta de dispositivos moviles y asi ayudar a varias personas a vender y/o  comprar lo que necesitemos'),
             decoration: new BoxDecoration(
-              image: new DecorationImage(image:new AssetImage("assets/images/img.jpg"),
+              image: new DecorationImage(image:new AssetImage("assets/images/menu.jpg"),
               fit: BoxFit.cover
               
               ),
