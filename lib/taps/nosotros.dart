7b2402c36047a38a35d52d76aa2692620faa28c9
unknown <you@example.com>
2019-04-23 06:49:28 -0400

    proyecto Terminado

--- lib/taps/nosotros.dart
+++ lib/taps/nosotros.dart
@@ -16,8 +16,8 @@ body: new Stack(
           //agregamos una imagen
           new Container(
             margin: const EdgeInsets.all(10.0), //margenes por todos lados
-            width: 400.0, //ancho de la img
-            height: 300.0, //largo de la img
+            width: 350.0, //ancho de la img
+            height: 350.0, //largo de la img
             child: new Text('Hernan Luis Villegas Llanco \n\n\nEste proyecto fue realizado para la compra y venta de dispositivos moviles y asi ayudar a varias personas a vender y/o  comprar lo que necesitemos'),
             decoration: new BoxDecoration(
               image: new DecorationImage(image:new AssetImage("assets/images/menu.jpg"),
